b8627672b9266758cad7b0c2a922d8db59012a8a
gisellavika2-dot <gisellavika2@gmail.com>
2025-09-21 14:59:06 +0700

    Add files via upload

---
+++ LAB_WEEK_04-master/app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.lab_week_04
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+
+val TABS_FIXED = listOf(
+    R.string.starbucks_title,
+    R.string.janjijiwa_title,
+    R.string.kopikenangan_title,
+)
+
+
+val TAB_DESC = listOf(
+    R.string.starbucks_desc,
+    R.string.janjijiwa_desc,
+    R.string.kopikenangan_desc,
+)
+
+class CafeAdapter(private val fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = TABS_FIXED.size
+
+    override fun createFragment(position: Int): Fragment {
+        val context = fragment.requireContext()
+        val desc = context.getString(TAB_DESC[position])
+        return CafeDetailFragment.newInstance(desc)
+    }
+}
---
+++ LAB_WEEK_04-master/app/src/main/java/com/example/lab_week_04/ListFragment.kt
@@ -0,0 +1,54 @@
+package com.example.lab_week_04
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.contextaware.OnContextAvailableListener
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+//class ListFragment : Fragment(), View.OnClickListener {
+class ListFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
+        )
+
+        coffeeList.forEach { coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(COFFEE_ID, coffee.id)
+            coffee.setOnClickListener {
+                Navigation.createNavigateOnClickListener(
+                    R.id.action_listFragment2_to_detailFragment, fragmentBundle
+                )
+            }
+        }
+    }
+    companion object{
+        const val COFFEE_ID = "COFFEE_ID"
+    }
+}
